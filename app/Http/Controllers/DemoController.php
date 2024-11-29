78e88c4743bf6e392f270ac7a174a1e0af34b33d
Noman Mia <nomanislamnishad@gmail.com>
2024-11-29 21:08:24 +0600

    added json body

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -4,15 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+//Request
 class DemoController extends Controller
 {
+   //simple request
    function DemoAction(){
     return response('this is my first laravel work');
    }
 
+  //Request url parameter
    function DemoSave(Request $request){
     $name =$request->name;
     $age =$request->age;
     return response("My name is {$name} and age is {$age}");
+   }
+
+   //Request json body
+   function DemoJsonBody(Request $request){
+     $name =$request->input("name");
+     $age =$request->input("age");
+     return response("my name is {$name}and age is {$age}");
    }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+//request
 Route::get('/about',[DemoController::class,"DemoAction"] );
 Route::get("/hello/{name}/{age}",[DemoController::class,"DemoSave"] );
+Route::get("/jsonBody",[DemoController::class,"DemoJsonBody"] );
